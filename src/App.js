ff5e1d770be8e1f5fe1c38e61da22cafd2491ba1
Mohamed Aziz Bechaib <azizbechaib711@gmail.com>
2023-06-07 22:23:10 +0100

    UI

--- src/App.js
+++ src/App.js
@@ -3,8 +3,42 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <div className="res"></div>
-            <div className="op"></div>
+            <div className="res">
+                <h1>Calculator</h1>
+                <input readOnly></input>
+                <input readOnly></input>
+            </div>
+            <hr />
+            <div className="op">
+                <div className="col">
+                    <button>AC</button>
+                    <button>7</button>
+                    <button>4</button>
+                    <button>1</button>
+                    <button>x</button>
+                </div>
+                <div className="col">
+                    <button>Del</button>
+                    <button>8</button>
+                    <button>5</button>
+                    <button>2</button>
+                    <button>0</button>
+                </div>
+                <div className="col">
+                    <button>%</button>
+                    <button>9</button>
+                    <button>6</button>
+                    <button>3</button>
+                    <button>.</button>
+                </div>
+                <div className="col">
+                    <button className="operator">/</button>
+                    <button className="operator">*</button>
+                    <button className="operator">-</button>
+                    <button className="operator">+</button>
+                    <button className="result">=</button>
+                </div>
+            </div>
         </div>
     );
 }
